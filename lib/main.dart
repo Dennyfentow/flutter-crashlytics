0822797d0bf9e76e37f62bc72abf3efc8930057c
Daniel dos Santos <daniel@automastersat.com.br>
2021-12-08 09:05:10 -0300

    parte01 - flutter e crashlytics funcionando juntos

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:alura_crashlytics/screens/dashboard.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  // Initialize Firebase.
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
   runApp(BytebankApp());
 }
 
 class BytebankApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
